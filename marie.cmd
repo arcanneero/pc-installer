8c74a2e76aaab3cc0cbfff357d5ea9f26e31f2af
Arcann <arcanneero@gmail.com>
2018-01-04 14:15:35 +0100

    init

---
+++ marie.cmd
@@ -0,0 +1,29 @@
+choco install -y googlechrome
+choco install -y libreoffice
+choco install -y officeproplus2013
+choco install -y adobereader
+choco install -y chocolateygui
+choco install -y --ignorechecksum teamviewer
+choco install -y spotify
+choco install -y audacity
+choco install -y winrar
+choco install -y vlc
+choco install -y notepadplusplus.install
+choco install -y atom
+choco install -y gimp
+choco install -y jre8
+choco install -y googledrive
+choco install -y git
+choco install -y tortoisegit
+choco install -y nodejs.install
+choco install -y putty.install
+choco install -y filezilla
+choco install -y keepass.install
+choco install -y yed
+choco install -y k-litecodecpackfull
+choco install -y --ignorechecksum battle.net
+choco install -y steam
+choco install -y slack
+choco install -y docker
+
+
---
+++ nicolas.cmd
@@ -0,0 +1,40 @@
+choco install -y googlechrome
+choco install -y libreoffice
+choco install -y adobereader
+choco install -y chocolateygui
+choco install -y --ignorechecksum teamviewer
+choco install -y spotify
+choco install -y audacity
+choco install -y winrar
+choco install -y vlc
+choco install -y notepadplusplus.install
+choco install -y atom
+choco install -y gimp
+choco install -y jdk6
+choco install -y jdk7
+choco install -y jdk8
+choco install -y jdk9
+choco install -y googledrive
+choco install -y git
+choco install -y tortoisegit
+choco install -y nodejs.install
+choco install -y putty.install
+choco install -y filezilla
+choco install -y keepass.install
+choco install -y yed
+choco install -y k-litecodecpackfull
+choco install -y --ignorechecksum battle.net
+choco install -y steam
+choco install -y steamcmd
+choco install -y slack
+choco install -y docker
+choco install -y intellijidea-ultimate
+choco install -y maven
+choco install -y visualstudiocode
+choco install -y yarn
+choco install -y eclipse
+choco install -y gradle
+choco install -y keepass-plugin-keeagent
+choco install -y superputty
+
+
